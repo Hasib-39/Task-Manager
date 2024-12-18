ae0a360c48f3ec5d9c4fc0dee3c7f478d7e79d00
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2024-12-18 23:25:32 +0600

    Screen Background and Sigin page touched

---
+++ lib/ui/screens/sign_in_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+  static const String name = '/sign-in';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScreenBackground(child: ),
+    );
+  }
+}
