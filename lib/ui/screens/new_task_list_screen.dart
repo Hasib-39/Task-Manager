e670830f268e175e051b4612cd4cd90a8b21e038
Hasib Altaf <hasibaltaf2839@gmail.com>
2025-01-08 18:04:17 +0600

    Task Manager Custom AppBar complete

--- lib/ui/screens/new_task_list_screen.dart
+++ lib/ui/screens/new_task_list_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/utils/app_colors.dart';
+
+import '../widgets/tm_app_bar.dart';
 
 class NewTaskListScreen extends StatefulWidget {
   const NewTaskListScreen({super.key});
@@ -10,8 +13,13 @@ class NewTaskListScreen extends StatefulWidget {
 class _NewTaskListScreenState extends State<NewTaskListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("New Task List Screen"),
+
+
+    return const Scaffold(
+      appBar: TMAppBar(),
     );
   }
 }
+
+
+
