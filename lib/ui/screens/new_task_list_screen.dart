fba8b16d659d815ceeab760676015f640a6a1cfb
Hasib Altaf <hasibaltaf2839@gmail.com>
2025-01-08 18:50:21 +0600

    Task Tile and Task status counter complete

--- lib/ui/screens/new_task_list_screen.dart
+++ lib/ui/screens/new_task_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/ui/utils/app_colors.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
 
+import '../widgets/task_item_widget.dart';
+import '../widgets/task_status_summary_counter_widget.dart';
 import '../widgets/tm_app_bar.dart';
 
 class NewTaskListScreen extends StatefulWidget {
@@ -15,11 +18,49 @@ class _NewTaskListScreenState extends State<NewTaskListScreen> {
   Widget build(BuildContext context) {
 
 
-    return const Scaffold(
+    return Scaffold(
       appBar: TMAppBar(),
+      body: ScreenBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _buildTasksSummaryByStatus(),
+              ListView.builder(
+                shrinkWrap: true,
+                  itemCount: 10,
+                  primary: false,
+                  itemBuilder: (context, index){
+                  return TaskItemWidget();
+                  }
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+  Widget _buildTasksSummaryByStatus() {
+    return const SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  TaskStatusSummaryCounterWidget(count: '09', title: 'New',),
+                  TaskStatusSummaryCounterWidget(count: '09', title: 'Progress',),
+                  TaskStatusSummaryCounterWidget(count: '09', title: 'Completed',),
+                  TaskStatusSummaryCounterWidget(count: '09', title: 'Cancelled',)
+                ],
+              ),
+            ),
+          );
+  }
 }
+
+
+
+
 
 
 
