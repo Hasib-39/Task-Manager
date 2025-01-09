66b5273d3f7e7fc6553ba4d05f9a0ac66eceba9c
Hasib Altaf <hasibaltaf2839@gmail.com>
2025-01-09 23:05:11 +0600

    Registration API call successfully added

--- lib/ui/screens/sign_up_screen.dart
+++ lib/ui/screens/sign_up_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager/data/services/network_caller.dart';
+import 'package:task_manager/data/utils/urls.dart';
 import 'package:task_manager/ui/screens/sign_in_screen.dart';
+import 'package:task_manager/ui/widgets/centered_circular_progress_indicator.dart';
 import 'package:task_manager/ui/widgets/screen_background.dart';
+import 'package:task_manager/ui/widgets/snack_bar_message.dart';
 
 import '../utils/app_colors.dart';
 
@@ -20,7 +24,7 @@ class _SignInScreenState extends State<SignUpScreen> {
   final TextEditingController _lastNameTEController = TextEditingController();
   final TextEditingController _mobileTEController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
+  bool _signUpInProgress = false;
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
@@ -46,6 +50,12 @@ class _SignInScreenState extends State<SignUpScreen> {
                       decoration: const InputDecoration(
                         hintText: 'Email',
                       ),
+                      validator: (String? value){
+                        if(value?.trim().isEmpty ?? true){
+                          return 'Enter your email';
+                        }
+                        return null;
+                      },
                     ),
                     const SizedBox(height: 8,),
                     TextFormField(
@@ -54,6 +64,12 @@ class _SignInScreenState extends State<SignUpScreen> {
                       decoration: const InputDecoration(
                         hintText: 'First Name',
                       ),
+                      validator: (String? value){
+                        if(value?.trim().isEmpty ?? true){
+                          return 'Enter your first name';
+                        }
+                        return null;
+                      },
                     ),
                     const SizedBox(height: 8,),
                     TextFormField(
@@ -62,6 +78,12 @@ class _SignInScreenState extends State<SignUpScreen> {
                       decoration: const InputDecoration(
                         hintText: 'Last Name',
                       ),
+                      validator: (String? value){
+                        if(value?.trim().isEmpty ?? true){
+                          return 'Enter your last name';
+                        }
+                        return null;
+                      },
                     ),
                     const SizedBox(height: 8,),
                     TextFormField(
@@ -70,6 +92,12 @@ class _SignInScreenState extends State<SignUpScreen> {
                       decoration: const InputDecoration(
                         hintText: 'Mobile',
                       ),
+                      validator: (String? value){
+                        if(value?.trim().isEmpty ?? true){
+                          return 'Enter your mobile no';
+                        }
+                        return null;
+                      },
                     ),
                     const SizedBox(height: 8,),
                     TextFormField(
@@ -78,23 +106,35 @@ class _SignInScreenState extends State<SignUpScreen> {
                       decoration: const InputDecoration(
                         hintText: 'Password',
                       ),
+                      validator: (String? value){
+                        if(value?.trim().isEmpty ?? true){
+                          return 'Enter your password';
+                        }
+                        return null;
+                      },
                     ),
                     const SizedBox(height: 24,),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColors.themeColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            fixedSize: const  Size.fromWidth(double.maxFinite),
-                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                            foregroundColor: Colors.white,
-                            textStyle: const TextStyle(
-                                fontSize: 16
-                            )
-                        ),
-                        onPressed: (){},
-                        child: const Icon(Icons.arrow_circle_right_outlined, color: Colors.white)
+                    Visibility(
+                      visible: _signUpInProgress == false,
+                      replacement: const CenteredCircularProgressIndicator(),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.themeColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              fixedSize: const  Size.fromWidth(double.maxFinite),
+                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                              foregroundColor: Colors.white,
+                              textStyle: const TextStyle(
+                                  fontSize: 16
+                              )
+                          ),
+                          onPressed: (){
+                      
+                          },
+                          child: const Icon(Icons.arrow_circle_right_outlined, color: Colors.white)
+                      ),
                     ),
                     const SizedBox(height: 48,),
                     Center(
@@ -107,6 +147,46 @@ class _SignInScreenState extends State<SignUpScreen> {
           )
       ),
     );
+  }
+
+  void _onTapSignUpButton(){
+    if(_formKey.currentState!.validate()){
+      _registerUser();
+    }
+  }
+
+  Future<void> _registerUser() async {
+    _signUpInProgress = true;
+    setState(() {
+
+    });
+    Map<String, dynamic> requestParams = {
+      "email":_emailTEController.text.trim(),
+      "firstName":_firstNameTEController.text.trim(),
+      "lastName":_lastNameTEController.text.trim(),
+      "mobile":_mobileTEController.text.trim(),
+      "password":_passwordTEController.text,
+      "photo":""
+    };
+    final NetworkResponse response = await NetworkCaller.postRequest(url: Urls.registrationUrl, body: requestParams);
+    _signUpInProgress = false;
+    setState(() {
+      
+    });
+    if(response.isSuccess){
+      _clearTextFields();
+      showSnackBarMessage(context, "Registration Successful");
+    }else{
+      showSnackBarMessage(context,response.errorMessage);
+    }
+  }
+
+  void _clearTextFields(){
+    _firstNameTEController.clear();
+    _lastNameTEController.clear();
+    _passwordTEController.clear();
+    _mobileTEController.clear();
+    _emailTEController.clear();
   }
   Widget _buildSignInSection(){
     return RichText(
